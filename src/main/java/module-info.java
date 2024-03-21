2e972262a2009bc67bf69339f8facdfd9c4fa92f
Mykhailo Kuryk <113907200+mishaa16@users.noreply.github.com>
2024-03-21 18:29:17 +0200

    Revert "add Fractals laboratory work!"
    This reverts commit 30ec1f5d0c0520493f4e4c13dfe6eb4f4383f1f0.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module org.study.labs.cg.computergraphics {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.desktop;
-    requires javafx.swing;
 
 
     opens org.study.labs.cg.computergraphics to javafx.fxml;
--- src/main/java/org/study/labs/cg/computergraphics/BezierCurveEditor.java
+++ src/main/java/org/study/labs/cg/computergraphics/BezierCurveEditor.java
@@ -1,4 +1,4 @@
-package org.labs.csg.csg2;
+package org.study.labs.cg.computergraphics;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,11 +10,7 @@ import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -26,18 +22,14 @@ public class BezierCurveEditor extends Application {
     private double tStep = 0.01;
     public static final String FILE_NAME = "coefficient_matrix.txt";
     private Point2D draggedPoint = null;
-    private BezierCalculationMethod calculationMethod = BezierCalculationMethod.PARAMETRIC;
 
-
-    // Метод, що ініціалізує графічний інтерфейс користувача
     @Override
     public void start(Stage primaryStage) {
         Canvas canvas = new Canvas(800, 600);
         primaryStage.setResizable(false);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-
         drawGrid(gc);
-        drawAxes(gc);
+
         createMouseActions(canvas, gc);
         MenuBar menuBar = createMenu(gc);
 
@@ -50,27 +42,13 @@ public class BezierCurveEditor extends Application {
         primaryStage.show();
     }
 
-    // Метод, що малює контрольні точки на площині та їх координати
     private void drawControlPoints(GraphicsContext gc) {
         gc.setFill(Color.RED);
         for (Point2D point : controlPoints) {
             gc.fillOval(point.getX() - 3, point.getY() - 3, 6, 6);
         }
-
-        // Отримати текст з координатами всіх точок
-        StringBuilder coordinatesText = new StringBuilder();
-        for (int i = 0; i < controlPoints.size(); i++) {
-            Point2D point = controlPoints.get(i);
-            coordinatesText.append("Point ").append(i + 1).append(": (").append((int) point.getX()).append(", ").append((int) point.getY()).append(")\n\n");
-        }
-
-        // Відображення тексту з координатами
-        gc.setFill(Color.BLACK);
-        gc.fillText(coordinatesText.toString(), gc.getCanvas().getWidth() - 120, 20);
     }
 
-
-    // Метод, що малює криву Без'є на площині
     private void drawBezierCurve(GraphicsContext gc) {
         gc.setStroke(Color.BLUE);
         gc.setLineWidth(1.5);
@@ -78,12 +56,7 @@ public class BezierCurveEditor extends Application {
         if (controlPoints.size() > 1) {
             bezierCurvePoints.clear();
             for (double t = 0; t <= 1; t += tStep) {
-                Point2D point;
-                if (calculationMethod == BezierCalculationMethod.PARAMETRIC) {
-                    point = calculateBezierParametric(controlPoints, t);
-                } else {
-                    point = calculateBezierPoint(controlPoints, t);
-                }
+                Point2D point = calculateBezierPoint(controlPoints, t);
                 bezierCurvePoints.add(point);
             }
 
@@ -95,7 +68,6 @@ public class BezierCurveEditor extends Application {
         }
     }
 
-    // Метод, що малює характеристичний багатокутник на площині
     private void drawCharacteristicPolygon(GraphicsContext gc) {
         gc.setStroke(Color.DARKGRAY);
         gc.setLineWidth(1.5);
@@ -110,7 +82,6 @@ public class BezierCurveEditor extends Application {
         }
     }
 
-    // Метод, що розраховує точку на кривій Без'є для заданого параметру t
     private Point2D calculateBezierPoint(List<Point2D> points, double t) {
         int n = points.size() - 1;
         double[][] matrix = new double[2][n + 1];
@@ -131,80 +102,35 @@ public class BezierCurveEditor extends Application {
         return new Point2D(x, y);
     }
 
-    // Метод, що обчислює параметричне представлення кривої Безьє для вказаного параметру t
-    private Point2D calculateBezierParametric(List<Point2D> points, double t) {
-        double x = 0;
-        double y = 0;
-        int n = points.size() - 1;
-
-        for (int i = 0; i <= n; i++) {
-            double b = binomialCoefficient(n, i) * Math.pow(t, i) * Math.pow(1 - t, n - i);
-            x += points.get(i).getX() * b;
-            y += points.get(i).getY() * b;
-        }
-
-        return new Point2D(x, y);
-    }
-
-    // Метод, що обчислює біноміальний коефіцієнт
     private int binomialCoefficient(int n, int k) {
         return factorial(n) / (factorial(k) * factorial(n - k));
     }
 
-    // Метод, що обчислює факторіал числа
     private int factorial(int n) {
         if (n <= 1)
             return 1;
         return n * factorial(n - 1);
     }
 
-    // Запис матриці коефіцієнтів в файл
     private void saveCoefficientMatrix() {
         try {
             FileWriter writer = new FileWriter(FILE_NAME);
-            int n = controlPoints.size() - 1;
-
             for (Point2D point : controlPoints) {
                 writer.write(point.getX() + ", " + point.getY() + "\n");
             }
             writer.write("\n");
-
-            for (int i = 0; i <= n; i++) {
-                for (int j = 0; j <= n; j++) {
-                    int binomial = binomialCoefficient(n, j);
-                    double coefficient = binomial * Math.pow(1 - (double) i / n, n - j) * Math.pow((double) i / n, j);
-                    writer.write(coefficient + " ");
-                }
-                writer.write("\n");
+            for (Point2D point : bezierCurvePoints) {
+                writer.write(point.getX() + ", " + point.getY() + "\n");
             }
-
             writer.close();
         } catch (IOException e) {
             throw new RuntimeException("Can't write coefficients to file: " + FILE_NAME);
         }
     }
 
-    //Створення меню для функцій
     private MenuBar createMenu(GraphicsContext gc) {
         MenuBar menuBar = new MenuBar();
         Menu menu = new Menu("Options");
-
-        // Створення пункту меню для вибору методу обчислення кривої Безьє
-        Menu calculationMethodMenu = new Menu("Calculation Method");
-        MenuItem parametricMenuItem = new MenuItem("Parametric");
-        MenuItem matrixMenuItem = new MenuItem("Matrix");
-        calculationMethodMenu.getItems().addAll(parametricMenuItem, matrixMenuItem);
-        menu.getItems().add(calculationMethodMenu);
-
-        parametricMenuItem.setOnAction(event -> {
-            calculationMethod = BezierCalculationMethod.PARAMETRIC;
-            redrawCanvas(gc);
-        });
-
-        matrixMenuItem.setOnAction(event -> {
-            calculationMethod = BezierCalculationMethod.MATRIX;
-            redrawCanvas(gc);
-        });
 
         MenuItem clearMenuItem = new MenuItem("Clear Canvas");
         clearMenuItem.setOnAction(event -> {
@@ -212,7 +138,6 @@ public class BezierCurveEditor extends Application {
             bezierCurvePoints.clear();
             gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
             drawGrid(gc);
-            drawAxes(gc);
         });
 
         MenuItem computeMenuItem = new MenuItem("Compute Bezier Curve with Step");
@@ -235,7 +160,6 @@ public class BezierCurveEditor extends Application {
                     } else {
                         gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
                         drawGrid(gc);
-                        drawAxes(gc);
                         drawControlPoints(gc);
                         drawCharacteristicPolygon(gc);
                         drawBezierCurve(gc);
@@ -257,34 +181,28 @@ public class BezierCurveEditor extends Application {
         return menuBar;
     }
 
-    // Метод, що встановлює дії для миші на полотні
     private void createMouseActions(Canvas canvas, GraphicsContext gc) {
         canvas.setOnMouseClicked(event -> {
             double x = event.getX();
             double y = event.getY();
-            // Перевірка, щоб курсор не виходив за межі канвасу
-            if (x >= 0 && x <= canvas.getWidth() && y >= 0 && y <= canvas.getHeight()) {
-                boolean overPoint = false;
-                for (Point2D point : controlPoints) {
-                    if (Math.abs(point.getX() - x) <= 3 && Math.abs(point.getY() - y) <= 3) {
-                        overPoint = true;
-                        break;
-                    }
+            boolean overPoint = false;
+            for (Point2D point : controlPoints) {
+                if (Math.abs(point.getX() - x) <= 3 && Math.abs(point.getY() - y) <= 3) {
+                    overPoint = true;
+                    break;
                 }
-                if (!overPoint) {
-                    controlPoints.add(new Point2D(x, y));
-                    gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
-                    drawGrid(gc);
-                    drawAxes(gc);
-                    drawControlPoints(gc);
-                    drawCharacteristicPolygon(gc);
-                    drawBezierCurve(gc);
-                    saveCoefficientMatrix();
-                }
+            }
+            if (!overPoint) {
+                controlPoints.add(new Point2D(x, y));
+                gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+                drawGrid(gc);
+                drawControlPoints(gc);
+                drawCharacteristicPolygon(gc);
+                drawBezierCurve(gc);
+                saveCoefficientMatrix();
             }
         });
 
-        // Дії під час руху курсором миші
         canvas.setOnMouseMoved(event -> {
             double x = event.getX();
             double y = event.getY();
@@ -302,7 +220,6 @@ public class BezierCurveEditor extends Application {
             }
         });
 
-        // Дії під час утримання курсору
         canvas.setOnMousePressed(event -> {
             double x = event.getX();
             double y = event.getY();
@@ -315,30 +232,24 @@ public class BezierCurveEditor extends Application {
             }
         });
 
-        // Дії під час утримання та руху курсором
         canvas.setOnMouseDragged(event -> {
             if (draggedPoint != null) {
                 double x = event.getX();
                 double y = event.getY();
-                // Перевірка, щоб курсор не виходив за межі канвасу
-                if (x >= 0 && x <= canvas.getWidth() && y >= 0 && y <= canvas.getHeight()) {
-                    if (controlPoints.contains(draggedPoint)) {
-                        int index = controlPoints.indexOf(draggedPoint);
-                        controlPoints.set(index, new Point2D(x, y));
-                        draggedPoint = new Point2D(x, y);
-                        gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
-                        drawGrid(gc);
-                        drawAxes(gc);
-                        drawControlPoints(gc);
-                        drawCharacteristicPolygon(gc);
-                        drawBezierCurve(gc);
-                        saveCoefficientMatrix();
-                    }
+                if (controlPoints.contains(draggedPoint)) {
+                    int index = controlPoints.indexOf(draggedPoint);
+                    controlPoints.set(index, new Point2D(x, y));
+                    draggedPoint = new Point2D(x, y);
+                    gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+                    drawGrid(gc);
+                    drawControlPoints(gc);
+                    drawCharacteristicPolygon(gc);
+                    drawBezierCurve(gc);
+                    saveCoefficientMatrix();
                 }
             }
         });
 
-        // Дії після відпускання курсору
         canvas.setOnMouseReleased(event -> {
             if (draggedPoint != null) {
                 draggedPoint = null;
@@ -347,47 +258,6 @@ public class BezierCurveEditor extends Application {
         });
     }
 
-    // Метод, що малює осі X та Y разом з розміткою
-    private void drawAxes(GraphicsContext gc) {
-        double canvasWidth = gc.getCanvas().getWidth();
-        double canvasHeight = gc.getCanvas().getHeight();
-
-        // Ось X
-        gc.setStroke(Color.BLACK);
-        gc.strokeLine(0, gc.getCanvas().getHeight() / 2, gc.getCanvas().getWidth(), gc.getCanvas().getHeight() / 2);
-
-        // Ось Y
-        gc.strokeLine(gc.getCanvas().getWidth() / 2, 0, gc.getCanvas().getWidth() / 2, gc.getCanvas().getHeight());
-
-        // Розмітка по осі X
-        double pixelSpacingX = 20; // Інтервал між пікселями
-        for (double x = gc.getCanvas().getWidth() / 2 + pixelSpacingX; x < gc.getCanvas().getWidth(); x += pixelSpacingX) {
-            gc.strokeLine(x, gc.getCanvas().getHeight() / 2 - 5, x, gc.getCanvas().getHeight() / 2 + 5);
-        }
-        for (double x = gc.getCanvas().getWidth() / 2 - pixelSpacingX; x > 0; x -= pixelSpacingX) {
-            gc.strokeLine(x, gc.getCanvas().getHeight() / 2 - 5, x, gc.getCanvas().getHeight() / 2 + 5);
-        }
-        gc.strokeLine(canvasWidth - 10, canvasHeight / 2 - 5, canvasWidth, canvasHeight / 2);
-        gc.strokeLine(canvasWidth - 10, canvasHeight / 2 + 5, canvasWidth, canvasHeight / 2);
-
-
-        // Розмітка по осі Y
-        double pixelSpacingY = 20; // Інтервал між пікселями
-        for (double y = gc.getCanvas().getHeight() / 2 + pixelSpacingY; y < gc.getCanvas().getHeight(); y += pixelSpacingY) {
-            gc.strokeLine(gc.getCanvas().getWidth() / 2 - 5, y, gc.getCanvas().getWidth() / 2 + 5, y);
-        }
-        for (double y = gc.getCanvas().getHeight() / 2 - pixelSpacingY; y > 0; y -= pixelSpacingY) {
-            gc.strokeLine(gc.getCanvas().getWidth() / 2 - 5, y, gc.getCanvas().getWidth() / 2 + 5, y);
-        }
-        gc.strokeLine(canvasWidth / 2 - 5, 10, canvasWidth / 2, 0);
-        gc.strokeLine(canvasWidth / 2 + 5, 10, canvasWidth / 2, 0);
-
-        // Позначення осей
-        gc.fillText("Y", gc.getCanvas().getWidth() / 2 + 10, 10);
-        gc.fillText("X", gc.getCanvas().getWidth() - 10, gc.getCanvas().getHeight() / 2 + 15);
-    }
-
-    // Малювання розмітки на площині
     private void drawGrid(GraphicsContext gc) {
         gc.setStroke(Color.LIGHTGRAY);
         for (double x = 20; x < gc.getCanvas().getWidth(); x += 20) {
@@ -396,22 +266,6 @@ public class BezierCurveEditor extends Application {
         for (double y = 20; y < gc.getCanvas().getHeight(); y += 20) {
             gc.strokeLine(0, y, gc.getCanvas().getWidth(), y);
         }
-    }
-
-    // Метод, що перемалює площину
-    private void redrawCanvas(GraphicsContext gc) {
-        gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
-        drawGrid(gc);
-        drawAxes(gc);
-        drawControlPoints(gc);
-        drawCharacteristicPolygon(gc);
-        drawBezierCurve(gc);
-        saveCoefficientMatrix();
-    }
-
-    private enum BezierCalculationMethod {
-        PARAMETRIC,
-        MATRIX
     }
 
     public static void main(String[] args) {
