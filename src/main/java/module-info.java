30ec1f5d0c0520493f4e4c13dfe6eb4f4383f1f0
Mykhailo Kuryk <113907200+mishaa16@users.noreply.github.com>
2024-03-21 18:28:33 +0200

    add Fractals laboratory work!

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.study.labs.cg.computergraphics {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires javafx.swing;
 
 
     opens org.study.labs.cg.computergraphics to javafx.fxml;
